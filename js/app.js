207b450ce451973993f0297ef2146027f886bbc6
SteeshMcK <stesha.norstrom@pcc.edu>
2020-04-21 23:02:04 -0700

    add styling to css file

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container")
 const authors = [
-  { name: "Horace Walpole", image: "images/horace-walpole.jpg" },
+  { name: "Horace Walpole", image: "images/horace-walpole.jpg", work: "The Castle of Otranto (1764)" },
   { name: "Clara Reeve", image: "images/clara-reeve.jpg" },
   { name: "Sophia Lee", image: "images/sophia-lee.jpg" },
   { name: "William Beckford", image: "images/william-beckford.jpg" },
@@ -11,11 +11,13 @@ const authors = [
 const showAuthors = () => {
     let output = ""
     authors.forEach(
-      ({ name, image }) =>
+      ({ name, image, work }) =>
         (output += `
                 <div class="card">
                   <img class="card--avatar" src=${image} />
                   <h1 class="card--title">${name}</h1>
+                  <p>Most notable work: </p>
+                  <p class="card--data">${work}</p>
                   <a class="card--link" href="#">Explore</a>
                 </div>
                 `)
