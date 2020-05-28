0f5768a12fd89f8610bce18d9d75cd2826092178
SteeshMcK <stesha.norstrom@pcc.edu>
2020-05-28 11:22:12 -0700

    add comments to service worker cache and install event

--- serverWorker.js
+++ serverWorker.js
@@ -1,3 +1,7 @@
+// serviceWorker.js => in which we cache the assets
+
+// staticGothic => name of cache
+// assets => the assets to be stored in the cache
 const staticGothic = "18th-century-gothic-site-v1"
 const assets = [
   "/",
@@ -14,9 +18,15 @@ const assets = [
   "/images/ann-radcliffe.jpg"
 ]
 
+// self => service worker
+// "install" is the first of the service worker life cycles. Runs when a service worker is installed.
+// When install event is fired, callback in run to give access to event object
 self.addEventListener("install", installEvent => {
+  // waitUntill() waits for "install" to finish since it is an asynchronous event.
   installEvent.waitUntil(
+    // Once cache API is ready, open() is run and creates cache
     caches.open(staticGothic).then(cache => {
+      // returns a promise that allows assets to be stored in the cache.
       cache.addAll(assets)
     })
   )
